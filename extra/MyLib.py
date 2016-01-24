c0a2070d2ff28e2f81b059a1ae52ba948eaea076
Eddie Gurnee <pegurnee@gmail.com>
2016-01-24 07:39:16 -0500

    got to get the most frequent

--- extra/MyLib.py
+++ extra/MyLib.py
@@ -1,5 +1,5 @@
 def minimum(arr):
-  _min = arr.pop()
+  _min = arr[0]
   for _elem in arr:
     if _min > _elem:
       _min = _elem
@@ -7,7 +7,8 @@ def minimum(arr):
   return _min
 
 def maximum(arr):
-  _max = arr.pop()
+  _max = arr[0]
+
   for _elem in arr:
     if _max < _elem:
       _max = _elem
@@ -15,18 +16,44 @@ def maximum(arr):
   return _max
 
 def mean(arr):
-  pass
-
-def median(arr):
-  pass
-
-def mode(arr):
-  pass
-
-def stdev(arr):
-  pass
+  return sum(arr) / len(arr)
 
 def sum(arr):
+  _total = 0
+
+  for _elem in arr:
+    _total += _elem
+
+  return _total
+
+def median(arr):
+  _len = len(arr)
+  _arr = sort(arr)
+
+  if _len % 2 == 0:
+    _ret = mean(_arr[int(_len / 2) - 1 : int(_len / 2) + 1])
+  else:
+    _ret = _arr[int(_len / 2)]
+
+  return _ret
+
+def sort(arr):
+  return sorted(arr)
+
+def mode(arr):
+  _listing = {}
+
+  for _elem in arr:
+    if _elem in _listing:
+      _listing[_elem] += 1
+    else:
+      _listing[_elem] = 1
+
+  sorted(_listing, key=lambda _item: )
+  print(_listing)
+  print(sorted(_listing))
+
+def stdev(arr):
   pass
 
 def percentile(arr, percent):
@@ -52,5 +79,11 @@ def jacard(arrU, arrV):
 
 
 if __name__ == '__main__':
-  print(min([1,2,5,-3,7,3.0]))
+  a =[2.5,5,-3,1.1,1,7,3,2.5]
+  print(sorted(a))
+  print('min = %s' % minimum(a))
+  print('max = %s' % maximum(a))
+  print('med = %s' % median(a))
+  print('avg = %s' % mean(a))
+  mode(a)
   # main()
