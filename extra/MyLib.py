8505f85c8deadbcedb85173f01e239ee25b97ba1
Eddie Gurnee <pegurnee@gmail.com>
2016-01-24 06:36:14 -0500

    add some max/min

--- extra/MyLib.py
+++ extra/MyLib.py
@@ -1,10 +1,33 @@
-def min(arr):
+def minimum(arr):
   _min = arr.pop()
   for _elem in arr:
     if _min > _elem:
       _min = _elem
 
   return _min
+
+def maximum(arr):
+  _max = arr.pop()
+  for _elem in arr:
+    if _max < _elem:
+      _max = _elem
+
+  return _max
+
+def mean(arr):
+  pass
+
+def median(arr):
+  pass
+
+def mode(arr):
+  pass
+
+def stdev(arr):
+  pass
+
+def sum(arr):
+  pass
 
 
 if __name__ == '__main__':
