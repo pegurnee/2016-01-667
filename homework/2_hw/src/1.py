a909a75250f7a19cbe1ff96e3ddcc75f37b58a84
Eddie Gurnee <pegurnee@gmail.com>
2016-02-19 22:42:35 -0500

    h2p1

--- homework/2_hw/src/1.py
+++ homework/2_hw/src/1.py
@@ -1,20 +1,36 @@
+import math
+
 def gini(arr):
   _vals = [(x / sum(arr)) ** 2 for x in arr]
-  print(_vals)
-  print(sum(_vals))
   return 1 - sum(_vals)
 
+def logb(val):
+  return math.log(val, 2)
 
-def _entropy(_set):
-  _gini = 0
-
-  _n = sum(_set)
-  for _s in _set:
-    _gini += (_s / _n) ** 2
-
-  return 1 - _gini
+def shannon(arr):
+  tot = sum(arr)
+  _vals = [(x / tot * logb(x / tot)) for x in arr]
+  return -sum(_vals)
 
 if __name__ == '__main__':
   a = [6, 10, 4]
 
+  _tot = sum(a)
+  _vals = [(x / _tot) ** 2 for x in a]
+  print(_vals)
+  print(sum(_vals))
+
   print(gini(a))
+
+  _vals = [(x / _tot * logb(x / _tot)) for x in a]
+  print(_vals)
+
+  print(shannon(a))
+
+  g1 = gini([5, 5, 5])
+  g2 = gini([6, 5, 4])
+  g3 = gini([10, 3, 2])
+  ginis = [g1, g2, g3]
+
+  print(ginis)
+  print(max(ginis))
