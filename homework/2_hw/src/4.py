aa73bb1322534c67c71ac28fb3d390c8c9030c4f
Eddie Gurnee <pegurnee@gmail.com>
2016-02-21 14:37:29 -0500

    h2p4 complete

--- homework/2_hw/src/4.py
+++ homework/2_hw/src/4.py
@@ -1,48 +1,47 @@
 from collections import Counter
 
-records = []
-
 def gini(arr):
   return 1 - sum([(x / sum(arr)) ** 2 for x in arr])
 
-def class_entropy(loc):
+def class_entropy(loc, record_set):
   _sum = 0
-  for recs in group_by(loc):
-    print(recs)
-    print(recs.values())
-    print(sum(recs.values()))
-    _sum += gini(recs.values()) * sum(recs.values()) / len(records)
+  for recs in group_by(loc, record_set):
+    # print(recs)
+    # print(recs.values())
+    # print(sum(recs.values()))
+    _sum += gini(recs.values()) * sum(recs.values()) / len(record_set)
   # return gini(arr), len(arr)
   return _sum
 
-def group_by(loc):
+def group_by(loc, record_set):
   left  = []
   right = []
-  for rec in records:
+  for rec in record_set:
     if rec[loc] in ['college', 'smoker', 'married', 'female', 'retired']:
       right.append(rec[-1])
     else:
       left.append(rec[-1])
-  print('left: {}'.format(left))
-  print('right: {}'.format(right))
-  print(Counter(left))
-  print(Counter(right))
+  # print('left: {}'.format(left))
+  # print('right: {}'.format(right))
+  # print(Counter(left))
+  # print(Counter(right))
   return Counter(left), Counter(right)
 
 if __name__ == '__main__':
-  print('part 1')
+  records = []
+  print('\npart 1')
   with open('4_data_a.txt') as f:
     for line in f:
       records.append(line.split())
 
-  for i in range(4):
-    print(class_entropy(i))
+  for i in range(5):
+    print(class_entropy(i, records))
 
-  print('part 2')
+  print('\npart 2')
   records = []
   with open('4_data_b.txt') as f:
     for line in f:
       records.append(line.split())
 
-  for i in range(4):
-    print(class_entropy(i))
+  for i in range(5):
+    print(class_entropy(i, records))
