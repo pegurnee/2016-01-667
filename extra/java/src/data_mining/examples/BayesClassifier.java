2bc2c6fa6680c97174bef8a46eb51ae876782d88
Eddie Gurnee <pegurnee@gmail.com>
2016-03-08 22:32:03 -0500

    load training

--- extra/java/src/data_mining/examples/BayesClassifier.java
+++ extra/java/src/data_mining/examples/BayesClassifier.java
@@ -1,6 +1,9 @@
 package data_mining.examples;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class BayesClassifier 
 {
@@ -39,4 +42,37 @@ public class BayesClassifier
 		table = null;
 	}
 
+	public void loadTrainingData(String trainingFile) throws IOException
+	{
+		Scanner inFile = new Scanner(new File(trainingFile));
+		
+		numberRecords = inFile.nextInt();
+		numberAttributes = inFile.nextInt();
+		numberClasses = inFile.nextInt();
+		
+		attributeValues = new int[numberAttributes];
+		for (int i = 0; i < numberAttributes; i++) 
+			attributeValues[i] = inFile.nextInt();
+		
+		records = new ArrayList<Record>();
+		
+		for (int i = 0; i < numberRecords; i++) {
+			int[] attributeArray = new int[numberAttributes];
+			
+			for (int j = 0; j < numberAttributes; j++) 
+			{
+				String label = inFile.next();
+				attributeArray[j] = convert(label, j+1);
+			}
+			
+			String label = inFile.next();
+			int className = convert(label, numberAttributes);
+			
+			Record record = new Record(attributeArray, className);
+			
+			records.add(record);
+		}
+		
+		inFile.close();
+	}
 }
