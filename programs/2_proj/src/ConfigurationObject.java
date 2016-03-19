6b3a7006f2758cbce761328b1656647badf80b14
Eddie Gurnee <pegurnee@gmail.com>
2016-03-19 12:31:43 -0400

    add getLocations to configobject

--- programs/2_proj/src/ConfigurationObject.java
+++ programs/2_proj/src/ConfigurationObject.java
@@ -3,15 +3,23 @@ import java.io.Serializable;
 public class ConfigurationObject
 	implements Serializable {
 	private static ConfigurationObject instance = null;
-	/**
-	 *
-	 */
+
 	private static final long serialVersionUID = -1354273965361346550L;
 
 	private final String IN_FOLDER = "in/";
 	private final String OUT_FOLDER = "out/";
 
+	private final String STOCK_LOCATION = "sp500/";
+
 	private ConfigurationObject() {}
+
+	public String getFileInput(int section, int question, FileType type) {
+		return this.getFileLocation(section, question, type);
+	}
+
+	public String getFileOutput(int section, int question, FileType type) {
+		return this.getFileLocation(section, question, type);
+	}
 
 	public ConfigurationObject getInstance() {
 		if (instance == null) {
@@ -30,12 +38,27 @@ public class ConfigurationObject
 		return this.OUT_FOLDER + part;
 	}
 
+	private String getFileLocation(int section, int question, FileType type) {
+		String location = this.getLocationInput(section, question);
+		location += type.toString();
+		if (!this.needsStockMarketValues(section, question)) {
+			if (section < 3) {
+				location += (question - 1);
+			}
+		}
+		return location;
+	}
+
 	private String getSubfolder(int section, int question) {
 		String part = "part" + section + "/";
-		if ((section == 3) && (question == 2)) {
-			part = "sp500/";
+		if (this.needsStockMarketValues(section, question)) {
+			part = this.STOCK_LOCATION;
 		}
 		return part;
+	}
+
+	private boolean needsStockMarketValues(int section, int question) {
+		return (section == 3) && (question == 2);
 	}
 
 }
