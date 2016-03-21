11629e3d099bd7d5aa04daf566689db45d3a00ed
Eddie Gurnee <pegurnee@gmail.com>
2016-03-21 15:55:58 -0400

    java resume data converter

--- programs/2_proj/src/JavaResumeDataConverter.java
+++ programs/2_proj/src/JavaResumeDataConverter.java
@@ -4,15 +4,72 @@ public class JavaResumeDataConverter
 
 	@Override
 	public String convertFromNumericalValue(int value, int column) {
-		// TODO Auto-generated method stub
+		String label;
 
-		return null;
+		switch (column) {
+			case 0:
+			case 2:
+				label = Integer.toString(value);
+				break;
+			case 4:
+				label = Character.toString((char) (value + 'A'));
+				break;
+			case 1:
+				// java knowledge or no
+			case 5:
+				// hire or not
+				if (value == 0) {
+					label = "no";
+				} else {
+					if (column == 1) {
+						label = "java";
+					} else {
+						label = "hire";
+					}
+				}
+				break;
+			case 3:
+				// cs degree or other
+			default:
+				if (value == 0) {
+					label = "cs";
+				} else {
+					label = "other";
+				}
+				break;
+		}
+
+		return label;
 	}
 
 	@Override
 	public int convertToNumericalValue(String label, int column) {
-		// TODO Auto-generated method stub
-		return 0;
+		int value;
+
+		switch (column) {
+			case 0:
+			case 2:
+				value = Integer.parseInt(label);
+				break;
+			case 4:
+				value = label.charAt(0) - 'A';
+				break;
+			case 1:
+				// java knowledge or not
+			case 3:
+				// cs degree or other
+			case 5:
+				// hire or not
+			default:
+				if ("no".equals(label) || "cs".equals(label)) {
+					value = 0;
+				} else {
+					value = 1;
+				}
+				break;
+		}
+
+		return value;
 	}
 
 }
