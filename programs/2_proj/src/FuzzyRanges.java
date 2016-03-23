a47eaec97125ce715a15170848bc4e0deffa7d6d
Eddie Gurnee <pegurnee@gmail.com>
2016-03-23 08:49:33 -0400

    arraylistsoflists

--- programs/2_proj/src/FuzzyRanges.java
+++ programs/2_proj/src/FuzzyRanges.java
@@ -1,5 +1,7 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.Scanner;
 
 public class FuzzyRanges {
@@ -11,7 +13,7 @@ public class FuzzyRanges {
 		Scanner inFile;
 		for (int i = 0; i < numFiles; i++) {
 			int numberRanges = 0;
-			double[][] values;
+			ArrayList<LinkedList<Double>> values;
 
 			for (int j = 0; j < fileTypes.length; j++) {
 				int numberRecords;
@@ -22,6 +24,11 @@ public class FuzzyRanges {
 
 				if (j == 0) {
 					numberRanges = inFile.nextInt() + inFile.nextInt();
+					values = new ArrayList<>(numberRanges);
+
+					for (int k = 0; k < values.size(); k++) {
+						values.add(new LinkedList<>());
+					}
 				}
 
 				for (int k = 0; k < numberRecords; k++) {
