8ae88b77ae04dc9fff20f6767b5fe81489b1d831
Eddie Gurnee <pegurnee@gmail.com>
2016-03-24 16:55:44 -0400

    S&P converter existing

---
+++ programs/2_proj/src/StandardAndPoorsNeuralNetDataConverter.java
@@ -0,0 +1,18 @@
+
+public class StandardAndPoorsNeuralNetDataConverter
+	implements NeuralNetworkDataConverterInterface {
+	private final int[] sapRange = { -2, 2 };
+
+	@Override
+	public String convertFromNumericalValue(double value, int column) {
+		// TODO Auto-generated method stub
+		return null;// ProjectTwoTools.;
+	}
+
+	@Override
+	public double convertToNumericalValue(String label, int column) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
