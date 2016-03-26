cb0ed0350537a38433aff3f508389de80622d67a
Eddie Gurnee <pegurnee@gmail.com>
2016-03-26 17:49:05 -0400

    in and out files

--- programs/3_proj/src/ConfigurationObject.java
+++ programs/3_proj/src/ConfigurationObject.java
@@ -1,7 +1,7 @@
 /**
  * This singleton object holds all of the data used to run and interact with
  * data mining project 3.
- * 
+ *
  * @author eddie
  *
  */
@@ -15,5 +15,17 @@ public class ConfigurationObject {
 		return instance;
 	}
 
+	private final int[][] FILE_NAMES = { { '1', '2' }, { '3', '4' } };
+	private final String IN_FOLDER = "in/";
+	private final String OUT_FOLDER = "out/";
+
 	private ConfigurationObject() {}
+
+	public String getInFile(int part, int question) {
+		return this.IN_FOLDER + this.FILE_NAMES[part - 1][question - 2];
+	}
+
+	public String getOutFile(int part, int question) {
+		return this.OUT_FOLDER + this.FILE_NAMES[part - 1][question - 2];
+	}
 }
