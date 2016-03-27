8c16b9c4d4b8343f5ae9068616343f90420346f0
Eddie Gurnee <pegurnee@gmail.com>
2016-03-27 18:16:05 -0400

    cluster algorithm

--- extra/java/src/data_mining/examples/Graph.java
+++ extra/java/src/data_mining/examples/Graph.java
@@ -1,6 +1,9 @@
 package data_mining.examples;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Graph 
 {
@@ -31,5 +34,52 @@ public class Graph
 		records = null;
 		matrix = null;
 		clusters = null;
+	}
+	
+	public void load(String inputFile) throws FileNotFoundException
+	{
+		Scanner inFile = new Scanner(new File(inputFile));
+		
+		numberRecords = inFile.nextInt();
+		numberAttributes = inFile.nextInt();
+		
+		records = new ArrayList<Record>();
+		
+		for (int i = 0; i < numberRecords; i++) {
+			double[] attributes = new double[numberAttributes];
+			for (int j = 0; j < numberAttributes; j++)
+				attributes[j] = inFile.nextDouble();
+			
+			Record record = new Record(attributes);
+			
+			records.add(record);
+		}
+		
+		inFile.close();
+	}
+	
+	public void setParameters(double delta)
+	{
+		this.delta = delta;
+	}
+	
+	public void cluster() 
+	{
+		createMatrix();
+		initializeClusters();
+		
+		int index = 0;
+		int clusterName = 0;
+		
+		while (index < numberRecords)
+		{
+			if (clusters[index] == -1)
+			{
+				assignCluster(index, clusterName);
+				clusterName = clusterName + 1;
+			}
+			
+			index = index + 1;
+		}
 	}
 }
