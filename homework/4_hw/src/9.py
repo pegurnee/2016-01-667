3e5333b74fa76a264a6cf3ee8d1c43bbf37287bd
Eddie Gurnee <pegurnee@gmail.com>
2016-03-29 11:07:01 -0400

    add boundary and cores

--- homework/4_hw/src/9.py
+++ homework/4_hw/src/9.py
@@ -30,3 +30,39 @@ def centroid(points):
     _sums[i] = s / len(points)
 
   return tuple(_sums)
+
+def is_core(point, distance_threshold, mincount, points):
+  _num = 0
+  for p in points:
+    if (distance(p, point) < distance_threshold):
+      _num += 1
+  return True if _num >= mincount else False
+
+def is_boundary(point, distance_threshold, cores):
+  for c in cores:
+    if (distance(c, point) < distance_threshold):
+      return True
+  return False
+
+if __name__ == '__main__':
+  dist = 1
+  count = 3
+
+  ugly_cheating = []
+  cores = []
+
+  for p in dpoints:
+    if is_core(p, dist, count, dpoints):
+      cores.append(p)
+      ugly_cheating.append('core')
+    else:
+      ugly_cheating.append('')
+
+  for i,p in enumerate(dpoints):
+    if ugly_cheating[i] != 'core':
+      if is_boundary(p, dist, cores):
+        ugly_cheating[i] = 'boundary'
+      else:
+        ugly_cheating[i] = 'noise'
+
+  print(ugly_cheating)
