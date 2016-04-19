012ad78035e02fb233a0712bcb21aef7c8004b82
Eddie Gurnee <egurnee@emich.edu>
2016-04-19 16:12:11 -0400

    doig p7

---
+++ homework/5_hw/src/7.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+dataset = [
+    (10,22),
+    (15,29),
+    (12,27),
+    (13,24),
+    (20,33)
+]
+
+def mean_adjusted(matrix):
+    _means = []
+
+    for c in matrix[0]:
+        _means.append(0)
+
+    for r in matrix:
+        for i,c in enumerate(r):
+             _means[i] += c
+
+    _means = [ x/len(matrix) for x in _means]
+
+    new_data = []
+
+    for row in matrix:
+        new_data.append([ v - m for v,m in zip(row, _means) ])
+
+    return new_data
+
+if __name__ == '__main__':
+    print(mean_adjusted(dataset))
